ae25b72cf01a9f95d2bca28f16fcbcf5f6183026
Daniel Tapia <danieltapiamoreno@outlook.com>
2023-03-15 17:06:25 -0500

    :construction: Read get request param

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ app.get('/new-route', (req, res) => {
   res.send("I'm a new route");
 });
 
+app.get('/categories/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Category',
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+  });
+});
+
 app.listen(port, () => {
   console.log(`Listening in port ${port}`);
 });
