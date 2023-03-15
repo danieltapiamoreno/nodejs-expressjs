ead915829379af44e66ead1981c7c705514e8d3f
Daniel Tapia <danieltapiamoreno@outlook.com>
2023-03-15 17:05:24 -0500

    :construction: get endpoints

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("My app");
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('This is my server in express');
+});
+
+app.get('/new-route', (req, res) => {
+  res.send("I'm a new route");
+});
+
+app.listen(port, () => {
+  console.log(`Listening in port ${port}`);
+});
